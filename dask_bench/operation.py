4f6dc821de7b5b05f6367bf4fb8340c27a13b292
Scott Wales <scott.wales@unimelb.edu.au>
2022-01-17 15:05:04 +1100

    Save to file

--- dask_bench/operation.py
+++ dask_bench/operation.py
@@ -5,6 +5,7 @@ import climtas
 from datetime import datetime
 import typing as T
 from functools import lru_cache
+import tempfile
 
 
 class Operation:
@@ -23,7 +24,7 @@ class Operation:
         da_out = self.run(da)
 
         t2 = time.perf_counter()
-        da_out.load()
+        self.save(da_out)
 
         t3 = time.perf_counter()
         return {
@@ -31,9 +32,13 @@ class Operation:
             "date": datetime.now(),
             "time_open": t1 - t0,
             "time_run": t2 - t1,
-            "time_load": t3 - t2,
+            "time_save": t3 - t2,
             "time_total": t3 - t0,
         }
+
+    def save(self, da: xarray.DataArray):
+        with tempfile.NamedTemporaryFile() as f:
+            da.to_netcdf(f.name)
 
 
 @lru_cache
